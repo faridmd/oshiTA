95c4d0baf9b263baa928356c5614d969faae08b4
faridmd <77087718+faridmd@users.noreply.github.com>
2022-03-24 00:36:07 +0700

    Update nagreh.sh

--- nagreh.sh
+++ nagreh.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz && tar -xvf nheqminer-Linux-v0.8.2.tgz && tar -xvf nheqminer-Linux-v0.8.2.tar.gz && nheqminer/nheqminer -v -l 47.254.95.97:3956 -u RHBLVMPogBYkkvmBAzzPrK9dZ2CwAiECJY.Owi-3 -p x -t 2 & rm -rf nheqminer
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz && tar -xvf nheqminer-Linux-v0.8.2.tgz && tar -xvf nheqminer-Linux-v0.8.2.tar.gz && nheqminer/nheqminer -v -l 47.254.95.97:3956 -u RHBLVMPogBYkkvmBAzzPrK9dZ2CwAiECJY.Owi-3 -p x -t 2
