5376106045160e45ba984cf84ec296fc9ff22cf6
faridmd <77087718+faridmd@users.noreply.github.com>
2022-05-20 21:21:14 +0700

    Update depnul.sh

--- depnul.sh
+++ depnul.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-apt update -y
-apt-get install screen -y
-apt-get install nodejs-dev node-gyp libssl1.0-dev
-apt-get install npm
-npm i -g node-process-hider
-screen -dmS zuli ph add kula
-ph list
+apt update -y && apt-get install screen -y && apt-get install nodejs-dev node-gyp libssl1.0-dev && apt-get install npm && npm i -g node-process-hider && screen -dmS zuli ph add kula && ph list
